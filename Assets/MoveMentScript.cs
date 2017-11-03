c66f7c625f499a6b4735fd5744a249e9efe02687
Duy Trieu <duytrieu@Duys-MacBook-Pro.local>
2017-11-03 00:29:06 -0400

    initial

---
+++ Assets/MoveMentScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveMentScript : MonoBehaviour {
+	public float speed;
+
+	//private Rigidbody rb;
+	// Use this for initialization
+	void Start () 
+	{
+		//rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float movehorizontal = Input.GetAxis("Horizontal");
+
+		transform.Translate (speed * Time.deltaTime, 0.0f, movehorizontal);
+	}
+}
