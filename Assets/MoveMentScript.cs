6962b05827f495696401ee1d13426e3f051e35a4
Duy Trieu <duytrieu@Duys-MacBook-Pro.local>
2017-11-04 03:12:34 -0400

    jump

--- Assets/MoveMentScript.cs
+++ Assets/MoveMentScript.cs
@@ -1,22 +1,69 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class MoveMentScript : MonoBehaviour {
+public class MoveMentScript : MonoBehaviour 
+{
 	public float speed;
 
-	//private Rigidbody rb;
-	// Use this for initialization
+	private float jump = 10f;
+
+	private float acceleration = 5f;
+
+	private float Velocity;
+
+	//private float hello;
+
+	//public LayerMask groundLayer;
+
+	//public BoxCollider col;
+
+	public Boolean IsJumping;
+
+	Vector3 movement;
+
+	//private Vector3 moveDirection = Vector3.zero;
+
+	private Rigidbody rb;
+
 	void Start () 
 	{
-		//rb = GetComponent<Rigidbody> ();
-	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-		float movehorizontal = Input.GetAxis("Horizontal");
+		rb = GetComponent<Rigidbody> ();
+		IsJumping = false;
 
-		transform.Translate (speed * Time.deltaTime, 0.0f, movehorizontal);
+		//col = GetComponent<BoxCollider> ();
 	}
+
+	void FixedUpdate ()
+	{
+
+		float moveHorizontal = Input.GetAxisRaw ("Horizontal");
+
+		Velocity = speed + (acceleration * Time.deltaTime);
+	
+		movement.Set (Velocity * Time.deltaTime, 0.0f, -moveHorizontal);
+
+		movement = movement.normalized * Velocity * Time.deltaTime;
+
+		rb.MovePosition (transform.position + movement);
+	
+		if (Input.GetKeyDown (KeyCode.Space) && IsJumping == false) {
+			//hello = rb.position.y;
+			rb.AddForce (Vector3.up * jump, ForceMode.Impulse);
+			IsJumping = true;
+		}
+
+
+	}
+	void OnCollisionEnter (Collision other)
+	{
+		if (other.gameObject.CompareTag ("Ground")) 
+		//if(rb.position.y == hello)
+		{
+			IsJumping = false;
+			//hello = -1;
+		}
+	}
+
 }
